4717cc63232541397f418330fa23ef58ac8e7deb
developersrinu <132943635+developersrinu@users.noreply.github.com>
2023-06-28 00:47:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	return key in sampleObject;
+	for(pp in sampleObject){
+		if(pp===key){
+			return true;
+		}
+	}
+	return false
 }
 
 // Do not change the code below
